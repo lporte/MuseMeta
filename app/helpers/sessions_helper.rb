c7d66bd87518875f1069d23cf3a494e919c255d3
Lauryn Porte <laurynamandaporte@gmail.com>
2015-08-01 19:43:12 -0700

    Add logged_in? and current_user functions to helper methods

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -4,7 +4,17 @@ helpers do
 	  session[:user_id] = user.id
 	end
 
-	def logout(user)
+	def logout
 	  session[:user_id] = nil
+	end
+
+	def logged_in?
+	  !!session[:user_id]
+	end
+
+	def current_user
+		if session[:user_id]
+		  User.find_by(id: session[:user_id])
+		end
 	end
 end
