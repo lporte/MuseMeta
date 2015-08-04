c9feb74f572b0a47b4b6ffddefbea0a3a3016a92
Lauryn Porte <laurynamandaporte@gmail.com>
2015-08-03 20:45:03 -0700

    Add search bar feature

--- app/controllers/search.rb
+++ app/controllers/search.rb
@@ -5,12 +5,13 @@ end
 
 get '/search/results_for/:search_request' do
 	@search_request = params[:search_request]
-	@results = (search_artists(@search_request) ).uniq
-	# + search_firstnames(@search_request) + search_lastnames(@search_request) + search_emails(@search_request)
-	if request.xhr?
-		content_type :json
-	(erb :"search/results", layout: false, locals: {results: @results}).to_json
-	else
+	@results = (
+		search_artwork(@search_request) + 
+		search_title(@search_request) + 
+		search_museum(@search_request) + 
+		search_username(@search_request) + 
+		search_artist(@search_request) 
+		).uniq
+	@attr_array  = [] 
 		erb :"search/results"
-	end
 end
--- app/helpers/search_helpers.rb
+++ app/helpers/search_helpers.rb
@@ -1,17 +1,21 @@
 helpers do
-	def search_artists(artist)
-		@artwork = Artwork.where('artist LIKE ?', "%#{artist}%")
+	def search_artwork(title)
+		@result = Artwork.where('title LIKE ?', "%#{title}%")
 	end
 
-	# def search_title(title)
-	# 	@artwork = Artwork.where('title LIKE ?', "%#{title}%")
-	# end
+	def search_title(title)
+		@result = Artwork.where('title LIKE ?', "%#{title}%")
+	end
 
-	# def search_museum(museum)
-	# 	@artwork = Artwork.where('museum LIKE ?', "%#{museum}%")
-	# end
+	def search_museum(name)
+		@result = Museum.where('name LIKE ?', "%#{name}%")
+	end
 
-	# def search_username(username)
-	# 	@artwork = Artwork.where('username LIKE ?', "%#{username}%")
-	# end
+	def search_username(username)
+		@result = User.where('username LIKE ?', "%#{username}%")
+	end
+
+	def search_artist(name)
+		@result = Artist.where('name LIKE ?', "%#{name}%")
+	end
 end
