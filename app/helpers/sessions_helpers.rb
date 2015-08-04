0edcc01fd37afa2fb4eb92fff3da3773ba8b3b72
Lauryn Porte <laurynamandaporte@gmail.com>
2015-08-03 20:45:28 -0700

    Edit session helper

--- app/helpers/sessions_helpers.rb
+++ app/helpers/sessions_helpers.rb
@@ -13,8 +13,9 @@ helpers do
 	end
 
 	def current_user
-		if session[:user_id]
-		  User.find_by(id: session[:user_id])
-		end
+		@current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
+		# if session[:user_id]
+		#   User.find_by(id: session[:user_id])
+		# end
 	end
 end
