582c2cf972070a94a1750c351c9f05f3a2c85ef7
Lauryn Porte <laurynamandaporte@gmail.com>
2015-08-11 08:43:07 -0700

    Add Artwork and User spec tests

--- spec/auth_sessions/auth_sessions_spec.rb
+++ spec/auth_sessions/auth_sessions_spec.rb
@@ -1,7 +1,31 @@
-# TO DO:
-# Test if user can Login
-#  - Does user have new session
-# Test if user can Sign Up
-#  - Is user info validated?
-# Test if user can Logout
-#  - Is session set to nil?
+require_relative 'spec_helper'
+
+describe "User Controller" do
+
+  describe 'GET /' do
+    it "responds with an OK status" do
+      get '/users'
+      expect(last_response.status).to eq(200)
+    end
+  end
+
+  describe 'POST /users' do
+    it "is redirecting to correct users/:id" do
+      post "/users", {username: "Steve", password: "ForURHealth" }
+      expect(last_response.redirect?).to be true
+      follow_redirect!
+      last_request.path == "/users/#{User.last.id}"
+    end
+
+    it "creates a new user in the db" do
+      #given
+      count_before = User.count
+      #when
+      post "/users", {username: "Steve", password: "ForURHealth" }
+      #then
+      count_after = User.count
+      expect(count_after).to be(count_before + 1)
+    end
+  end
+
+end
