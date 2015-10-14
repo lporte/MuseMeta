6b7deb9ec7f9a59c86d39980a5faa68ac9064f0a
Lauryn Amanda Porte <lporte@users.noreply.github.com>
2015-10-13 22:59:28 -0700

    Format artworks.rb

--- app/controllers/artworks.rb
+++ app/controllers/artworks.rb
@@ -1,33 +1,33 @@
 #----------------SHOW ALL-----------------
 
 get '/artworks' do
-    @artworks = Artwork.all.order(id: :asc)
-	if logged_in?
-		erb :'artworks/all'
+  @artworks = Artwork.all.order(id: :asc)
+    if logged_in?
+      erb :'artworks/all'
 	else
-		redirect '/'
-	end
+      redirect '/'
+    end
 end
 
 #-----------------CREATE-----------------
 
 get '/artworks/new' do
-	@artwork = Artwork.new
-	erb :'artworks/new'
+  @artwork = Artwork.new
+  erb :'artworks/new'
 end
 
 post '/artworks' do
-	@artwork = Artwork.new(
-		title: params[:title],
-		source: params[:source],
-		date: params[:date]
-		)
-	if @artwork.save
-		redirect "/artworks/#{@artwork.id}"
-	else
-		status 400
-		erb :'artworks/new'
-	end
+  @artwork = Artwork.new(
+    title: params[:title],
+    source: params[:source],
+    date: params[:date]
+    )
+  if @artwork.save
+    redirect "/artworks/#{@artwork.id}"
+  else
+    status 400
+    erb :'artworks/new'
+  end
 end
 
 #----------------SHOW----------------
@@ -40,25 +40,24 @@ end
 #----------------EDIT----------------
 
 get '/artworks/:id/edit' do
-	@artwork = Artwork.find(params[:id])
-	erb :"artworks/edit"
+  @artwork = Artwork.find(params[:id])
+  erb :"artworks/edit"
 end
 
 put '/artworks/:id' do
-	@artwork = Artwork.find(params[:id])
-	@artwork.update_attributes(
-	  	title: params[:title],
-	  	artist: params[:artist],
-	  	url: params[:url],
-	)
-	# @artwork.save
-	redirect "/artworks/#{@artwork.id}"
+  @artwork = Artwork.find(params[:id])
+  @artwork.update_attributes(
+    title: params[:title],
+    artist: params[:artist],
+    url: params[:url],
+    )
+   redirect "/artworks/#{@artwork.id}"
 end
 
 #----------------DELETE---------------
 
 delete '/artworks/:id' do
-	@artwork = Artwork.find(params[:id])
-	@artwork.destroy
+  @artwork = Artwork.find(params[:id])
+  @artwork.destroy
 	redirect "/artworks"
 end
